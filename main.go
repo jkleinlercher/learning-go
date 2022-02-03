25b1f40e2814e9ec3c438889d30ff33fef3bc291
Johannes Kleinlercher <johannes@kleinlercher.at>
2022-02-03 20:52:07 +0100

    added user input and slices

--- main.go
+++ main.go
@@ -3,5 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("hello world")
+	var conferenceName string = "Go Conference"
+	const conferenceTickets uint = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("Welcome to %s booking application!\n", conferenceName)
+	fmt.Printf("We have total of %d tickets and %d are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	var bookings []string
+
+	// ask user for their name
+
+	fmt.Print("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Print("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Print("Enter your email address: ")
+	fmt.Scan(&email)
+	fmt.Print("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all bookings: %v", bookings)
+
 }
